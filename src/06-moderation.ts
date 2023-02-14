f79099741898c1efe488a09f77f191096049203a
Anthony Lee <hososlch@gmail.com>
2023-02-14 14:14:24 +0800

    update: moderation

--- src/06-moderation.ts
+++ src/06-moderation.ts
@@ -1,15 +1,18 @@
 import {openai} from "./util/openai";
 
 (async () => {
-    const violence = await openai.createModeration({input: "信唔信我殺你全家？"});
+    const violence = await openai.createModeration({input: "信唔信我而家就殺咗你全家？"});
     console.info("Violence Example:");
-    console.log(JSON.stringify(violence.data.results, null, 4));
+    console.table(violence.data.results[0].categories)
+    console.table(violence.data.results[0].category_scores)
 
-    const sexual = await openai.createModeration({input: "妳好靚女又大波，我想同妳做愛"});
+    const sexual = await openai.createModeration({input: "妳好靚女又大波呀，可唔可以同妳做愛？"});
     console.info("Sexual Example:");
-    console.log(JSON.stringify(sexual.data.results, null, 4));
+    console.table(sexual.data.results[0].categories)
+    console.table(sexual.data.results[0].category_scores)
 
-    const selfHarm = await openai.createModeration({input: "人生冇意義了，不如死咗佢"});
+    const selfHarm = await openai.createModeration({input: "人生冇意義了，不如燒炭算"});
     console.info("Self-harm Example:");
-    console.log(JSON.stringify(selfHarm.data.results, null, 4));
+    console.table(selfHarm.data.results[0].categories)
+    console.table(selfHarm.data.results[0].category_scores)
 })();
